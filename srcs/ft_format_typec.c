1b7e214c7b96bbeab388b1f9e3bafabe64304cc5
toruneko1129 <kawakita.hayato@gmail.com>
2021-08-12 00:07:20 +0900

    updated

--- srcs/ft_format_typec.c
+++ srcs/ft_format_typec.c
@@ -1,5 +1,15 @@
 #include "../includes/ft_printf.h"
 
+static int	ft_typec_bufinit(t_fmt *fmt)
+{
+	fmt->buf2 = (char *)malloc((fmt->len2 + 1) * sizeof(char));
+	if (fmt->buf2 == NULL)
+		return (FAILED);
+	ft_memset(fmt->buf2, ' ', fmt->len2);
+	*(fmt->buf2 + fmt->len2) = '\0';
+	return (SUCCESS);
+}
+
 int	ft_fmt_typec(int res, va_list *ap, t_fmt *fmt)
 {
 	int		c;
@@ -7,13 +17,8 @@ int	ft_fmt_typec(int res, va_list *ap, t_fmt *fmt)
 	fmt->len2 = fmt->mfw;
 	if (!fmt->len2)
 		fmt->len2 = 1;
-	if (fmt->len >= INT_MAX - res - fmt->len2)
+	if (fmt->len >= INT_MAX - res - fmt->len2 || ft_typec_bufinit(fmt))
 		return (FAILED);
-	fmt->buf2 = (char *)malloc((fmt->len2 + 1) * sizeof(char));
-	if (fmt->buf2 == NULL)
-		return (FAILED);
-	ft_memset(fmt->buf2, ' ', fmt->len2);
-	*(fmt->buf2 + fmt->len2) = '\0';
 	c = va_arg(*ap, int);
 	if (fmt->minus)
 		*(fmt->buf2) = (unsigned char)c;
---
+++ srcs/ft_format_types.c
@@ -0,0 +1,8 @@
+#include "../includes/ft_printf.h"
+
+int	ft_fmt_types(int res, va_list *ap, t_fmt *fmt)
+{
+	char	*s;
+
+	s = va_arg(*ap, char *);
+}
