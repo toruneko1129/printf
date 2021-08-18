64b3de6ffde0805a175764b5867df4be1dfdd617
toruneko1129 <kawakita.hayato@gmail.com>
2021-08-18 21:57:52 +0900

    updated

--- run.sh
+++
@@ -1,8 +0,0 @@
-gcc -c test_printf.c
-gcc test_printf.o libftprintf.a
-./a.out lib > lib.txt
-./a.out > ft.txt
-diff -s lib.txt ft.txt
-#valgrind --leak-check=full --show-leak-kinds=all ./a.out
-rm test_printf.o a.out
-rm -rf a.out*
